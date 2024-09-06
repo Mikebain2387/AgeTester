2d56f82569ded4a812c4528b74905549baad8fb4
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-06 13:11:35 -0500

    update add confetti

--- app.js
+++ app.js
@@ -5,17 +5,63 @@
 
 let birthday = document.getElementById("bday");
 let calculate = document.getElementById("mathButton");
+let message = document.getElementById("message");
+const year = new Date().getFullYear();
 
-const date = new Date();
-console.log(date);
+
+
 
 calculate.addEventListener("click", calculateAge)
 
 function calculateAge(){
- if(birthday.input !== ""){
- const sum = date - birthday.input;
+ if(birthday.value !== ""){
+  let bdayYear = birthday.value;
+let age = year - bdayYear;
+
+if (age < 18){
+ message.innerHTML = (`You are only ${age} that is too young.  You can git out`);
+
+} else if(age >= 18){
+ message.innerHTML =(` You are ${age} Welcome to the chill zone!!`);
+ const duration = 15 * 1000,
+  animationEnd = Date.now() + duration,
+  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const interval = setInterval(function() {
+  const timeLeft = animationEnd - Date.now();
+
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+
+  const particleCount = 50 * (timeLeft / duration);
+
+  // since particles fall down, start a bit higher than random
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    })
+  );
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    })
+  );
+}, 150);
+ 
 
 }
-console.log(sum);
+clearInput()
+}
 
+}
+
+function clearInput(){
+ birthday.value="";
 }
